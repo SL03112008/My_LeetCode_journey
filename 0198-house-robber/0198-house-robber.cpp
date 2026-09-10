11e3ea4097a991c79fd28cdf4a9a1c2641c9b11e
SAI LIKHITH <svsailikhith@gmail.com>
2026-09-10 19:32:41 +0530

    Time: 3 ms (6.25%), Space: 13.1 MB (6.09%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,17 @@
+class Solution {
+private:
+    int f(vector<int> nums, int i,vector<int> &dp){
+        if(i<0) return 0;
+        if(i==0) return nums[i];
+        if(dp[i]!=-1) return dp[i];
+
+        int rob = nums[i] + f(nums,i-2,dp);
+        int skip = f(nums,i-1,dp);
+        return dp[i] = max(rob,skip);
+    }
+public:
+    int rob(vector<int>& nums) {
+        vector<int> dp(nums.size()+2,-1);
+        return f(nums,nums.size()-1,dp);
+    }
+};
